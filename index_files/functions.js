4c63a713c4da1444cc83e9d240f9654e24ffcdcb
ZHYCarge <40520292+ZHYCarge@users.noreply.github.com>
2023-12-29 19:50:16 +0800

    Add files via upload
    更新文件

--- index_files/functions.js
+++ index_files/functions.js
@@ -38,7 +38,34 @@ $(window).resize(function() {
 })(jQuery);
 
 function timeElapse(date){
-	var days = Math.floor(seconds / (3600 * 24));
-	var result = "第 <span class=\"digit\">" + days + "</span> 天";
+	var dayss = new Date(config.datearrive);
+	var today = new Date();
+	if (dayss - today > 0)
+	{
+		var seconds = (dayss.getTime()- today) / 1000;
+		var days = Math.floor(seconds / (3600 * 24));
+		seconds = seconds % (3600 * 24);
+		var hours = Math.floor(seconds / 3600);
+		if (days>0)
+			hours+=24;
+		if (hours < 10) {
+			hours = "0" + hours;
+		}
+		seconds = seconds % 3600;
+		var minutes = Math.floor(seconds / 60);
+		if (minutes < 10) {
+			minutes = "0" + minutes;
+		}
+		seconds = Math.floor(seconds % 60);
+		seconds = (seconds + "").replace(/\.\d*/, "");
+		var result = "还有 <span class=\"digit\">" + hours + "</span> 小时 <span class=\"digit\">" + minutes + "</span> 分钟 <span class=\"digit\">" + seconds + "</span> 秒";
+	}
+	else{
+		var seconds = (+ today - date.getTime()) / 1000;
+		var days = Math.floor(seconds / (3600 * 24));
+		var result = "第 <span class=\"digit\">" + days + "</span> 天";
+	}
+	console.log(dayss);
+
 	$("#clock").html(result);
 }
